28a29ed8a74d755522250db6dfbbe76056faf1f7
Marius Schlüter <themrslue@googlemail.com>
2022-05-20 22:21:06 +0200

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-mkdir data && wget https://dl.dropboxusercontent.com/s/3ew4q0yoptqcskf/data_road.zip && unzip data_road
+mkdir data && cd data && wget https://dl.dropboxusercontent.com/s/3ew4q0yoptqcskf/data_road.zip && unzip data_road
 pip install -r requirements.txt
