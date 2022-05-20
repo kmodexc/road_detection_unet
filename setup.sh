14cfcaa69e8602faf4a8a2e28327e690b3855c5c
Marius Schlüter <themrslue@googlemail.com>
2022-05-20 22:24:11 +0200

    cd back

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-mkdir data && cd data && wget https://dl.dropboxusercontent.com/s/3ew4q0yoptqcskf/data_road.zip && unzip data_road
+mkdir data && cd data && wget https://dl.dropboxusercontent.com/s/3ew4q0yoptqcskf/data_road.zip && unzip data_road.zip && cd ..
 pip install -r requirements.txt
